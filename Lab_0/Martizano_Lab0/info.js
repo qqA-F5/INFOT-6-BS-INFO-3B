6e6267fb0010d4bf65c9c3d3064d3cb9a8d41dcb
James John Martizano <163330395+aintjames@users.noreply.github.com>
2026-04-09 14:04:48 +0800

    Add files via upload

---
+++ Lab_0/Martizano_Lab0/info.js
@@ -0,0 +1,7 @@
+const name = "Paula";
+const course = "BS INFO 3B";
+const school = "UA Main";
+
+console.log("Student:", name);
+console.log("Course & Section:", course);
+console.log("School:", school);
