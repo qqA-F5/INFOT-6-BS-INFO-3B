4e0dc56fd4903831406ac96cbc79a34f13acedd8
Mel-Nipes <msnipes@antiquespride.edu.ph>
2026-04-10 12:23:38 +0800

    Delete Lab_1/local-api-lab/index.js

--- Lab_1/local-api-lab/index.js
+++
@@ -1,8 +0,0 @@
-const dayjs = require('dayjs');
-const now = dayjs();
-const formattedNow = now.format('MMMM D, YYYY');
-const nextWeek = now.add(7, 'day');
-const formattedNextWeek = nextWeek.format('MMMM D, YYYY');
-
-console.log("Current Date: ", formattedNow);
-console.log("Next week: ", formattedNextWeek);
